69e7e1e34c52690fc94598305b76c79583a42c37
kami2212 <110316254+kami2212@users.noreply.github.com>
2022-08-19 05:20:28 +0800

    Update Monster.cs

--- homework 3/Assets/Script/Monster.cs
+++ homework 3/Assets/Script/Monster.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 
 namespace Su
-{
+{   /// <summary>
+    /// 怪物欄位
+    /// </summary>
     public class Monster : MonoBehaviour
     {
-        [SerializeField,Header("移動速度"),Range(0,10)]
+        [SerializeField, Header("移動速度"), Range(0, 10), Tooltip("移動速度")]
         private float movespeed = 3.5f;
         [SerializeField, Tooltip("攻擊力"), Range(0, 500)]
         private int attack = 100;
@@ -12,12 +14,12 @@ namespace Su
         private int hp = 350;
         [SerializeField, Tooltip("追蹤範圍"), Range(0, 50)]
         private int trackingrange = 30;
-        [SerializeField, Tooltip("追蹤範圍")]
+        [SerializeField, Tooltip("追蹤位移")]
         private Vector3 trackdisplacement;
 
         [SerializeField, Header("掉落道具")]
         private GameObject dropprops;
-        [SerializeField, Tooltip("掉落機率"),Range(0,1)]
+        [SerializeField, Tooltip("掉落機率"), Range(0, 1)]
         private float dropchance = 1;
 
         [SerializeField, Header("掉落道具音效")]
@@ -27,9 +29,9 @@ namespace Su
         [SerializeField, Tooltip("攻擊音效")]
         private AudioClip attackaudio;
 
-        private AudioSource ads;
-        private Rigidbody2D rig;
-        private Animator ani;
+        private AudioSource ads;//音效來源 
+        private Rigidbody2D rig;//2D 剛體
+        private Animator ani;//動畫控制器
     }
 }
 
